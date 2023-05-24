6dca9ddcf5c8d094d2a0582cd34c7ba100f3e200
Kamirru <->
2023-05-24 19:36:49 +0200

    oblicz_pierwiastki function added

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print(2**7)
+def oblicz_pierwiastki(a, b, c):
+    delta = b**2 - 4*a*c
+    if delta < 0:
+        return None
+    elif delta == 0:
+        x = -b / (2*a)
+        return x
+    else:
+        x1 = (-b + delta**0.5) / (2*a)
+        x2 = (-b - delta**0.5) / (2*a)
+        return x1, x2
+
+if __name__ == '__main__':
+    postac_ogolna = (1,6,8)
+    print(oblicz_pierwiastki(*postac_ogolna))
